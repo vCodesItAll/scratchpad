c3bf8e1d1837158383999cdbd68c9ada18bdb016
Vanessa Dickey <vcdickey@gmail.com>
2023-10-10 19:12:17 -0400

    adding styles

---
+++ React/1.Quickstart/2.nested-components.js
@@ -0,0 +1,19 @@
+// react components are javascript functions that return markup
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
+}
+
+// now you've nested the MyButton component inside the MyApp component
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
+
+// React components start with capital letters i.e. MyButton
+// HTML tags start with lowercase letters i.e. button
