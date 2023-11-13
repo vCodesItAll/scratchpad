913d6adfe26e47cffe47ba4b93b823ddd3e6e698
Vanessa Dickey <vcdickey@gmail.com>
2023-11-13 16:24:13 -0500

    auth session code in prettier

--- prettier-file.js
+++ prettier-file.js
@@ -1,3 +1,25 @@
+
+function nbMonths(startPriceOld, startPriceNew, savingPerMonth, percentLossByMonth) {
+    let months = 0;
+    let savings = 0;
+    let oldCarValue = startPriceOld;
+    let newCarValue = startPriceNew;
+    let percentLoss = percentLossByMonth / 100;
+
+    while (savings + oldCarValue < newCarValue) {
+        months++;
+        if (months % 2 === 0) {
+            percentLoss += 0.005;
+        }
+        oldCarValue *= (1 - percentLoss);
+        newCarValue *= (1 - percentLoss);
+        savings += savingPerMonth;
+    }
+
+    const leftOver = Math.round(savings + oldCarValue - newCarValue);
+    return [months, leftOver];
+}
+
 /* 
 Parameters: 
 - (positive int or float, guaranteed) start_price_old (Old car price)
@@ -5,22 +27,21 @@ Parameters:
 - (positive int or float, guaranteed) saving_per_month 
 - (positive float or int, guaranteed) percent_loss_by_month
 Return: 
-  - one camel cased word
+  - two positive integers for months and leftoverMoney
 Example: 
-  - "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
-Concerns: 
-  - remember to capitalize the next letter when you take out the 
-  slashes/underscores
+  - nbMonths(2000, 8000, 1000, 1.5) should return [6, 766] or (6, 766)
+Code Defensively: 
+  - using math.round and not math.floor
+  - parameter types are guaranteed
 Explain: 
-  - I will write out the word but when I get to a dash or underscore I will
-  take it out and look at the next index and make it uppercase then I will
-  continue writing the letters in lowercase until I hit another dash or 
-  underscore
+  - With every passing month I will depreciate the new and old car by
+    1.5 percent each month. I will add 1000 to the saved money. Every 2 months I will depreciate both cars by
+    an additional 0.5 percent.  When the amount of saved money is equal to or greater than the new car's worth
+    minus the old car's worth I will calculate how many months have passed since I started and I will calculate
+    how much saved money we have after subtracted the old and new cars.
 Positive Self Talk: 
   - I have solved similar problems before, so I know I can figure this out :)
   I have the tools in my toolbox.
 Translate: 
-  - Going to loop through and print string and make conditions that when 
-  there is an underscore or dash I will use the toUpperCase method 
-  to make that index capital then continue printing the rest
+  - Going to translate this to code
   */
--- prettier-file.py
+++ prettier-file.py
@@ -1,44 +1,149 @@
-def to_camel_case(text):
-    # make a list so you can apply string methods
-    arr = list(text)
-    list_length = len(arr)
-    # iterate through each element except the last one
-    i = 0
-    while i < list_length - 1:
-        # if you see an underscore or dash
-        if arr[i] in ('_', '-'):
-            # make the letter after it capital
-            arr[i+1] = arr[i+1].upper()
-            # delete the underscore or dash            
-            arr.pop(i)
-            # decrement the list length to account for the removed element
-            list_length -= 1
-        else:
-            i += 1
-    return ''.join(arr)
+# import necessary modules
+from fastapi import FastAPI, Depends, HTTPException, status
+from database import engine
+from sqlalchemy import select
+from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
+# define constants
+SECRET_KEY = "293bc37dafdbd68e264988cb88aec5a97595d9590bd59617af5894013a63e408"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# define database
+db = {
+    "jdh": {
+        "username":"jdh",
+        "full_name":"Justin Hall",
+        "email":"jhalljhall@gmail.com",
+        "hashed_password": "$2b$12$X/9jkgQEy0HHBADsEhHtG.CRK5Ic/2WFsQ0bmb5rS8DZHUV7O1U8u",
+        "disabled":False
+    }
+}
 
-# /* 
-# Parameters: 
-#   - a string with dashes and underscores separating words
-# Return: 
-#   - one camel cased word
-# Example: 
-#   - "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
-# Concerns: 
-#   - remember to capitalize the next letter when you take out the 
-#   slashes/underscores
-# Explain: 
-#   - I will write out the word but when I get to a dash or underscore I will
-#   take it out and look at the next index and make it uppercase then I will
-#   continue writing the letters in lowercase until I hit another dash or 
-#   underscore
-# Positive Self Talk: 
-#   - I have solved similar problems before, so I know I can figure this out :)
-#   I have the tools in my toolbox.
-# Translate: 
-#   - Going to loop through and print string and make conditions that when 
-#   there is an underscore or dash I will use the toUpperCase method 
-#   to make that index capital then continue printing the rest
-#   */
+# define models
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+# define password functions
+pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+# define user functions
+def get_user(db, username: str):
+    if username in db:
+        user_data = db[username]
+        return UserInDB(**user_data)
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+# define token functions
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# define dependency functions
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username:str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise 
+    
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+    
+    return user
+
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive User")
+    return current_user
+
+# define app
+app = FastAPI()
+
+# define routes
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorect user/pass", headers={"WWW-Authenticate":"Bearer"})
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+
+    return {"access_token": access_token, "token_type":"bearer"}
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user:User = Depends(get_current_active_user)):
+    return current_user
+
+@app.get("/users/me/items")
+async def read_own_items(current_user:User = Depends(get_current_active_user)):
+    return [{"item_id":1, "owner": current_user}]
+
+# add CORS middleware
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# define test route
+@app.get("/test/")
+async def test():
+    return "hello"
+
+# pw = get_password_hash("jdh1974")
+# print(pw)
