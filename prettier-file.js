7d87380c22e08e341207c4ae8df9c9b86fedc0b6
Vanessa Dickey <vcdickey@gmail.com>
2023-11-27 15:47:43 -0500

    added auto imports

--- prettier-file.js
+++ prettier-file.js
@@ -1,47 +1,62 @@
+function scramble(str1, str2) {
+  // Create frequency maps for str1 and str2
+  const frequencyMap1 = {};
+  const frequencyMap2 = {};
 
-function nbMonths(startPriceOld, startPriceNew, savingPerMonth, percentLossByMonth) {
-    let months = 0;
-    let savings = 0;
-    let oldCarValue = startPriceOld;
-    let newCarValue = startPriceNew;
-    let percentLoss = percentLossByMonth / 100;
+  // Populate frequency map for str1
+  for (let char of str1) {
+     // Each time char appears increment its frequency value by 1 otherwise leave it as 0
+    frequencyMap1[char] = (frequencyMap1[char] || 0) + 1;
+  }
 
-    while (savings + oldCarValue < newCarValue) {
-        months++;
-        if (months % 2 === 0) {
-            percentLoss += 0.005;
-        }
-        oldCarValue *= (1 - percentLoss);
-        newCarValue *= (1 - percentLoss);
-        savings += savingPerMonth;
+  // Populate frequency map for str2
+  for (let char of str2) {
+    // Each time char appears increment its frequency value by 1 otherwise leave it as 0
+    frequencyMap2[char] = (frequencyMap2[char] || 0) + 1;
+  }
+
+  // Check if all characters in str2 can be formed from str1
+  for (let char of str2) {
+    // If char is not in str1 or its frequency is 0 then return false
+    if (!frequencyMap1[char] || frequencyMap1[char] === 0) {
+      return false;
     }
+    frequencyMap1[char]--;
+  }
 
-    const leftOver = Math.round(savings + oldCarValue - newCarValue);
-    return [months, leftOver];
+  return true;
 }
 
-/* 
-Parameters: 
-- (positive int or float, guaranteed) start_price_old (Old car price)
-- (positive int or float, guaranteed) start_price_new (New car price)
-- (positive int or float, guaranteed) saving_per_month 
-- (positive float or int, guaranteed) percent_loss_by_month
-Return: 
-  - two positive integers for months and leftoverMoney
-Example: 
-  - nbMonths(2000, 8000, 1000, 1.5) should return [6, 766] or (6, 766)
-Code Defensively: 
-  - using math.round and not math.floor
-  - parameter types are guaranteed
-Explain: 
-  - With every passing month I will depreciate the new and old car by
-    1.5 percent each month. I will add 1000 to the saved money. Every 2 months I will depreciate both cars by
-    an additional 0.5 percent.  When the amount of saved money is equal to or greater than the new car's worth
-    minus the old car's worth I will calculate how many months have passed since I started and I will calculate
-    how much saved money we have after subtracted the old and new cars.
-Positive Self Talk: 
-  - I have solved similar problems before, so I know I can figure this out :)
-  I have the tools in my toolbox.
-Translate: 
-  - Going to translate this to code
-  */
+// Parameters:
+// Two strings, str1 and str2, consisting of lowercase letters (a-z). 
+// These strings represent the input for comparison.
+
+// Return:
+// The function scramble(str1, str2) returns true if a portion of the 
+// characters in str1 can be rearranged to match str2. Otherwise, it returns false.
+
+// Example:
+// scramble('rkqodlw', 'world') // Returns True
+// scramble('cedewaraaossoqqyt', 'codewars') // Returns True
+// scramble('katas', 'steak') // Returns False
+// These examples demonstrate the expected outputs of the function for specific input cases.
+
+// Consider Edge Cases:
+// Ensure the function handles scenarios where str1 or str2 might be empty strings
+// or contain invalid characters (punctuation, digits, etc.). 
+// Note that the problem statement specifies that only lowercase letters (a-z) will be present.
+
+// Explain:
+// See whether it's possible to rearrange the characters in str1 to form str2. 
+// Consider the count of characters in str1 to determine if it's possible to create str2 by rearranging its characters.
+
+// Positive Self Talk:
+// This is the last of a beautiful bootcamp full of katas.  I have come so far and have so far yet to go.  
+// I am such a smart and kind individual and my creativity can find a solution to this problem and any other problem
+// that comes my way.  Thank you, Ember, for your guidance and patience. And for your time and for so much food and 
+// for cultivating an inclusive community for the greater good.  Thank you for being a positive role model.
+
+// Translate:
+// The function needs to efficiently determine whether the characters in str1 can be rearranged to form str2. 
+// It should account for the count and presence of characters in both strings to determine if it's feasible to 
+// create str2 by rearranging the characters in str1.
--- prettier-file.py
+++ prettier-file.py
@@ -1,149 +1,40 @@
-# import necessary modules
-from fastapi import FastAPI, Depends, HTTPException, status
-from database import engine
-from sqlalchemy import select
-from fastapi.responses import PlainTextResponse
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from passlib.context import CryptContext
+def comp(a, b):
+    # your code
+        if a is None or b is None or len(a) != len(b):
+            return False
+        
+        a.sort()
+        b.sort()
+        
+        for x in a:
+            if x**2 not in b:
+                return False
+        
+        return True
 
-# define constants
-SECRET_KEY = "293bc37dafdbd68e264988cb88aec5a97595d9590bd59617af5894013a63e408"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# Parameters:
+# Two arrays, 'a' and 'b', containing elements to be compared.
 
-# define database
-db = {
-    "jdh": {
-        "username":"jdh",
-        "full_name":"Justin Hall",
-        "email":"jhalljhall@gmail.com",
-        "hashed_password": "$2b$12$X/9jkgQEy0HHBADsEhHtG.CRK5Ic/2WFsQ0bmb5rS8DZHUV7O1U8u",
-        "disabled":False
-    }
-}
+# Return:
+# A function comp(a, b)
 
-# define models
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+# Example:
+# Valid arrays:
+# a = [121, 144, 19, 161, 19, 144, 19, 11]  
+# b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 
-class TokenData(BaseModel):
-    username: str | None = None
+# Consider Edge Cases:
+# Handle scenarios where arrays 'a' or 'b' might be empty ([] or {}), or contain nil, null, None, or nothing in specific languages. Ensure that if 'a' or 'b' are undefined, the function should return false.
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+# Explain:
 
-class UserInDB(User):
-    hashed_password: str
+# Compare the arrays 'a' and 'b' to determine if 'b' contains elements that are 
+# corresponding squares of elements in 'a'. 
 
-# define password functions
-pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
+# Positive Self Talk:
+# I am woman.  Hear me roar.
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-# define user functions
-def get_user(db, username: str):
-    if username in db:
-        user_data = db[username]
-        return UserInDB(**user_data)
-
-def authenticate_user(db, username: str, password: str):
-    user = get_user(db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-# define token functions
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp":expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-# define dependency functions
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username:str = payload.get("sub")
-        if username is None:
-            raise credential_exception
-        token_data = TokenData(username=username)
-
-    except JWTError:
-        raise 
-    
-    user = get_user(db, username=token_data.username)
-    if user is None:
-        raise credential_exception
-    
-    return user
-
-async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive User")
-    return current_user
-
-# define app
-app = FastAPI()
-
-# define routes
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorect user/pass", headers={"WWW-Authenticate":"Bearer"})
-
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-
-    return {"access_token": access_token, "token_type":"bearer"}
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(current_user:User = Depends(get_current_active_user)):
-    return current_user
-
-@app.get("/users/me/items")
-async def read_own_items(current_user:User = Depends(get_current_active_user)):
-    return [{"item_id":1, "owner": current_user}]
-
-# add CORS middleware
-origins = [
-    "http://localhost:8000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# define test route
-@app.get("/test/")
-async def test():
-    return "hello"
-
-# pw = get_password_hash("jdh1974")
-# print(pw)
+# Translate:
+# The function should iterate through both arrays, square each element in 'a', and compare if the 
+# squared elements exist in 'b'. If any element in 'b' doesn't match the squared elements in 'a', 
+# return false.
