a3298a9cf12c55d9d5410119023cf1f248d837a4
vCodesItAll <vcdickey@gmail.com>
2024-01-20 15:46:54 -0500

    new kata

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-function comp(a, b) {
-    if (!a || !b) {
-        return false;
-    }
-
-    const sortedA = a.sort((x, y) => x - y);
-    const sortedB = b.sort((x, y) => x - y);
-
-    for (let i = 0; i < sortedA.length; i++) {
-        if (sortedA[i] * sortedA[i] !== sortedB[i]) {
-            return false;
+function evalObject(value){
+        var result = 0;
+        switch(value.operation){
+            case'+': result = value.a + value.b; break;
+            case'-': result = value.a - value.b; break;
+            case'/': result = value.a / value.b; break;
+            case'*': result = value.a * value.b; break;
+            case'%': result = value.a % value.b; break;
+            case'^': result = Math.pow(value.a, value.b); break;
         }
-    }
-
-    return true;
+        return result;
 }
