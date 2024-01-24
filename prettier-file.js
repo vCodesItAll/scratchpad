9e066be385df8f4ca7b282080028568f0a7eb6f5
Vanessa Dickey <vcdickey@gmail.com>
2024-01-24 00:02:44 -0500

    new kata

--- prettier-file.js
+++ prettier-file.js
@@ -1,9 +1,10 @@
-function pigIt(str){
-  return str.split(' ').map(word => {
-    if (word.match(/[a-zA-Z]/)) {
-      return word.slice(1) + word[0] + 'ay';
-    } else {
-      return word;
-    }
-  }).join(' ');
+function findOdd(A) {
+  //happy coding!
+  return 0;
+}function findOdd(A) {
+  let result = 0;
+  for (let i = 0; i < A.length; i++) {
+    result ^= A[i];
+  }
+  return result;
 }
