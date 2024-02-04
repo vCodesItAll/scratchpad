70e5e9e28da6a68d92f4a219090b0947c3e5fcfe
Vanessa Dickey <vcdickey@gmail.com>
2024-02-04 21:42:14 +0000

    cleared js prettier

--- prettier-file.js
+++ prettier-file.js
@@ -1,16 +1,1 @@
 
-function getSum(a, b) {
-  let sum = 0;
-  if (a === b) {
-    return a;
-  } else if (a < b) {
-    for (let i = a; i <= b; i++) {
-      sum += i;
-    }
-  } else {
-    for (let i = b; i <= a; i++) {
-      sum += i;
-    }
-  }
-  return sum;
-}
