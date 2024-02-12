4d14078c29ba2fbaf659e1f1700055c8333d1970
Vanessa Dickey <vcdickey@gmail.com>
2024-02-12 18:53:37 -0500

    snail sort javascript kata complete

--- prettier-file.js
+++ prettier-file.js
@@ -1,16 +1,24 @@
-
-function getSum(a, b) {
-  let sum = 0;
-  if (a === b) {
-    return a;
-  } else if (a < b) {
-    for (let i = a; i <= b; i++) {
-      sum += i;
+snail = function(array) {
+  let result = [];
+  while (array.length) {
+    // Get the first row and add it to the result
+    result.push(...array.shift());
+    
+    // Get the last element from each remaining row and add them to the result
+    for (let i = 0; i < array.length; i++) {
+      result.push(array[i].pop());
     }
-  } else {
-    for (let i = b; i <= a; i++) {
-      sum += i;
+    
+    // Get the last row in reverse order and add it to the result
+    if (array.length) {
+      result.push(...array.pop().reverse());
+    }
+    
+    // Get the first element from each remaining row in reverse order and add them to the result
+    for (let i = array.length - 1; i >= 0; i--) {
+      result.push(array[i].shift());
     }
   }
-  return sum;
+  
+  return result;
 }
