f2a269d20fca0351096721d2a51c8220fbfb961f
vCodesItAll <vcdickey@gmail.com>
2024-02-26 01:39:35 -0500

    starting firebot first conditional

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function factorial(n) {
-    if (n < 0) return null;
-    let res = [1];
-    for (let i = 1; i <= n; i++) {
-        for (let j = 0, carry = 0; j < res.length || carry > 0; j++) {
-            carry += (res[j] || 0) * i;
-            res[j] = carry % 10;
-            carry = carry / 10 | 0;
-        }
-    }
-    return res.reverse().join('');
-}
+$customVariable[isFirst]
 
 
